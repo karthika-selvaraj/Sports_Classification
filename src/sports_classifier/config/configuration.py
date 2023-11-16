9da338cf7dfcca9a8f41a27540e74fef646afcf8
Karthika Selvaraj <karthikaselvaraj@Karthikas-MacBook-Air.local>
2023-11-16 18:42:51 +0530

    prepare callbacks added

--- src/sports_classifier/config/configuration.py
+++ src/sports_classifier/config/configuration.py
@@ -1,7 +1,9 @@
 from sports_classifier.constants import *
+import os
 from sports_classifier.utils.common import read_yaml, create_directories
 from sports_classifier.entity.config_entity import (DataIngestionConfig,
-                                                    PrepareBaseModelConfig)
+                                                    PrepareBaseModelConfig,
+                                                    PrepareCallbacksConfig)
 
 # Configuration Manager
 class ConfigurationManager:
@@ -43,3 +45,19 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+    def get_prepare_callback_config(self)-> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir = Path(config.root_dir),
+            tensorboard_root_log_dir = Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath = Path(config.checkpoint_model_filepath)
+        )
+
+        return prepare_callback_config
