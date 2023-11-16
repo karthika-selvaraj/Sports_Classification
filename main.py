4e1eb15f1fcd3169ec956720c6f0824cb72e3cc1
Karthika Selvaraj <karthikaselvaraj@Karthikas-MacBook-Air.local>
2023-11-16 16:56:16 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sports_classifier import logger
 from sports_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from sports_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -12,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e 
+
+
+STAGE_NAME = "Prepare base model"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
