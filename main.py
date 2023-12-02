76b98f4ba42fa8ef03c97d989fa1319f0bf5469e
Karthika Selvaraj <karthikaselvaraj@Karthikas-MacBook-Air.local>
2023-12-02 19:39:12 +0530

    training added

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from sports_classifier import logger
 from sports_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from sports_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from sports_classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
     logger.info(f">>>> stage {STAGE_NAME} is started <<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nX======X")
 except Exception as e:
     logger.exception(e)
@@ -19,9 +20,20 @@ STAGE_NAME = "Prepare base model"
 try:
     logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/sports_classifier/config/configuration.py
+++ src/sports_classifier/config/configuration.py
@@ -3,7 +3,8 @@ import os
 from sports_classifier.utils.common import read_yaml, create_directories
 from sports_classifier.entity.config_entity import (DataIngestionConfig,
                                                     PrepareBaseModelConfig,
-                                                    PrepareCallbacksConfig)
+                                                    PrepareCallbacksConfig,
+                                                    PrepareTrainingConfig)
 
 # Configuration Manager
 class ConfigurationManager:
@@ -61,3 +62,27 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+    def get_training_config(self) -> PrepareTrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "trainData")
+        
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = PrepareTrainingConfig(
+                root_dir = Path(training.root_dir),
+                trained_model_path = Path(training.trained_model_path),
+                updated_base_model_path = Path(prepare_base_model.updated_base_model_path),
+                training_data = Path(training_data),
+                params_epochs = params.EPOCHS,
+                params_batch_size = params.BATCH_SIZE,
+                params_is_augmentation = params.AUGUMENTATION,
+                params_image_size = params.IMAGE_SIZE
+        )
+
+        return training_config
